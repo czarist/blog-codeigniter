f29ff8f27d628356c6d8358d1d860470b5bad24b
Cezar  Trentin <czartrentin@gmail.com>
2021-01-14 01:54:10 -0300

    modeland view da Home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,37 +3,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
-
-  /**
-   * Index Page for this controller.
-   *
-   * Maps to the following URL
-   * 		http://example.com/index.php/welcome
-   *	- or -
-   * 		http://example.com/index.php/welcome/index
-   *	- or -
-   * Since this controller is set as the default controller in
-   * config/routes.php, it's displayed at http://example.com/
-   *
-   * So any other public methods not prefixed with an underscore will
-   * map to /index.php/welcome/<method_name>
-   * @see https://codeigniter.com/user_guide/general/urls.html
-   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
   public function index()
   {
-    $dados['mensagem'] = "home";
-    $this->load->view('home', $dados);
-  }
-  public function teste()
-  {
-    $dados['mensagem'] = "testando";
-    $this->load->view('home', $dados);
-  }
-
-  public function testedb()
-  {
-    $dados['mensagem'] = $this->db->get('postagens')->result();
-    echo '<pre>';
-    print_r($dados);
+    $this->load->view('frontend/template/html-header');
+    $this->load->view('frontend/template/header');
+    $this->load->view('frontend/home');
+    $this->load->view('frontend/template/aside');
+    $this->load->view('frontend/template/footer');
+    $this->load->view('frontend/template/html-footer');
   }
 }
--- application/views/frontend/template/footer.php
+++ application/views/frontend/template/footer.php
@@ -7,7 +7,7 @@
 <footer>
   <div class="row">
     <div class="col-lg-12">
-      <p>Copyright &copy; Blog do CodeIgniter <?php echo date('y'); ?></p>
+      <p>Copyright &copy; Blog do CodeIgniter <?php echo date('Y'); ?></p>
     </div>
     <!-- /.col-lg-12 -->
   </div>
