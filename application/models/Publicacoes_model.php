f2c30621072ee8833f296aabf6e1f0968e3c2313
Cezar  Trentin <czartrentin@gmail.com>
2021-01-14 03:17:52 -0300

    criando destaque da home

---
+++ application/models/Publicacoes_model.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Publicacoes_model extends CI_Model
+{
+
+  public $id;
+  public $categoria;
+  public $titulo;
+  public $subtitulo;
+  public $conteudo;
+  public $data;
+  public $img;
+  public $user;
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  public function destaques_home()
+  {
+    $this->db->limit(4);
+    $this->db->order_by('data', 'DESC');
+    return $this->db->get('postagens')->result();
+  }
+}
