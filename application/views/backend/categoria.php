eb2f93d0ffa5317161a8906310e5ab993008c0a4
Cezar  Trentin <czartrentin@gmail.com>
2021-01-15 01:21:04 -0300

    view categorias backend

--- application/views/backend/categoria.php
+++ application/views/backend/categoria.php
@@ -1,21 +1,54 @@
 <div id="page-wrapper">
   <div class="row">
     <div class="col-lg-12">
-      <h1 class="page-header"><?php echo $subtitulo; ?></h1>
+      <h1 class="page-header"><?php echo 'Administrar ' . $subtitulo; ?></h1>
     </div>
     <!-- /.col-lg-12 -->
   </div>
   <!-- /.row -->
   <div class="row">
-    <div class="col-lg-12">
+    <div class="col-lg-6">
       <div class="panel panel-default">
         <div class="panel-heading">
-          <?php echo $subtitulo; ?>
+          <?php echo 'adicionar nova ' . $subtitulo; ?>
         </div>
         <div class="panel-body">
           <div class="row">
             <div class="col-lg-12">
-              <form role="form">
+            </div>
+          </div>
+          <!-- /.row (nested) -->
+        </div>
+        <!-- /.panel-body -->
+      </div>
+      <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-6 -->
+    <div class="col-lg-6">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <?php echo 'Alterar ' . $subtitulo . ' existente'; ?>
+        </div>
+        <div class="panel-body">
+          <div class="row">
+            <div class="col-lg-12">
+            </div>
+          </div>
+          <!-- /.row (nested) -->
+        </div>
+        <!-- /.panel-body -->
+      </div>
+      <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-6 -->
+  </div>
+  <!-- /.row -->
+</div>
+<!-- /#page-wrapper -->
+
+</div>
+
+<!-- <form role="form">
                 <div class="form-group">
                   <label>Titulo</label>
                   <input class="form-control" placeholder="Entre com o texto">
@@ -41,20 +74,4 @@
                 </div>
                 <button type="submit" class="btn btn-default">Cadastrar</button>
                 <button type="reset" class="btn btn-default">Limpar</button>
-              </form>
-            </div>
-
-          </div>
-          <!-- /.row (nested) -->
-        </div>
-        <!-- /.panel-body -->
-      </div>
-      <!-- /.panel -->
-    </div>
-    <!-- /.col-lg-12 -->
-  </div>
-  <!-- /.row -->
-</div>
-<!-- /#page-wrapper -->
-
-</div>
+              </form> -->
